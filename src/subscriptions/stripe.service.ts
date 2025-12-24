739570ec5ea76821d207df4b27690c3417c83d59
Jason A Savage <32180237+TheSavageDev@users.noreply.github.com>
2025-12-23 21:02:25 -0600

    fix build

--- src/subscriptions/stripe.service.ts
+++ src/subscriptions/stripe.service.ts
@@ -7,7 +7,7 @@ export class StripeService {
 
   constructor() {
     this.stripe = new Stripe(process.env.STRIPE_SECRET_KEY || '', {
-      apiVersion: '2025-11-17.clover',
+      apiVersion: '2025-12-15.clover',
     });
   }
 
@@ -59,7 +59,7 @@ export class StripeService {
     });
   }
 
-  async handleWebhook(payload: string, signature: string) {
+  handleWebhook(payload: string, signature: string) {
     return this.stripe.webhooks.constructEvent(
       payload,
       signature,
